bb62691c49bad0bdaa32ae5e37cdacfe127a72a9
Tural Novruzov <tural.nt.1@gmail.com>
2020-12-16 19:02:34 +0400

    Added password choose validation

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,19 @@
-const sections = ["#section-email", "#section-personal"];
+const sections = ["#section-email", "#section-personal", "#section-password-choose"];
 let activeSectionIdx = 0;
+
+const passwordRegex = /^[0-9]{6}$/;
+
+let password;
+
+function moveSection(n) {
+    $(sections[activeSectionIdx]).addClass("d-none");
+    activeSectionIdx += n;
+    $(sections[activeSectionIdx]).removeClass("d-none");
+}
 
 function nextSection(event) {
     if (event.target.checkValidity()) {
-        $(sections[activeSectionIdx]).addClass("d-none");
-        $(sections[++activeSectionIdx]).removeClass("d-none");
+        moveSection(1);
     } else {
         event.target.classList.add('was-validated');
     }
@@ -14,13 +23,30 @@ function nextSection(event) {
 }
 
 function prevSection(event) {
-    $(sections[activeSectionIdx]).addClass("d-none");
-    $(sections[--activeSectionIdx]).removeClass("d-none");
+    moveSection(-1);
+
+    event.preventDefault();
+}
+
+function passwordChooseNext(event) {
+    const passwordInput = $("#password-choose-input");
+
+    if (passwordRegex.test(passwordInput.val())) {
+        passwordInput.removeClass("is-invalid");
+        password = passwordInput.val();
+
+        moveSection(1);
+    } else {
+        passwordInput.addClass("is-invalid");
+    }
 
     event.preventDefault();
 }
 
 $(document).ready(() => {
-    $(".section").submit(nextSection);
+    $("#form-email").submit(nextSection);
+    $("#form-personal").submit(nextSection);
+    $("#form-password-choose").submit(passwordChooseNext);
     $(".goback-button").click(prevSection);
+
 });
