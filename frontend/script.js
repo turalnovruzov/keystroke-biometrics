19c95e188c30977f9bd0b983c90710f647d5e234
Tural Novruzov <tural.nt.1@gmail.com>
2020-12-16 14:52:18 +0400

    Added email validation

--- frontend/script.js
+++ frontend/script.js
@@ -2,10 +2,15 @@ const sections = ["#section-email", "#section-personal"];
 let activeSectionIdx = 0;
 
 function nextSection(event) {
-    $(sections[activeSectionIdx]).addClass("d-none");
-    $(sections[++activeSectionIdx]).removeClass("d-none");
+    if (event.target.checkValidity()) {
+        $(sections[activeSectionIdx]).addClass("d-none");
+        $(sections[++activeSectionIdx]).removeClass("d-none");
+    } else {
+        event.target.classList.add('was-validated');
+    }
 
     event.preventDefault();
+
 }
 
 function prevSection(event) {
