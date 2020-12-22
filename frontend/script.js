b82168b82694f52a9de9e82e20344a69441abbc2
Tural Novruzov <tural.nt.1@gmail.com>
2020-12-22 19:46:59 +0400

    Added keydown data collection

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,9 @@ const passwordRegex = /^[0-9]{6}$/;
 let activeSectionIdx = 3;
 let password;
 
+let passwordKeystrokes = [];
+let passwordKeystrokesTmp = [];
+
 function moveSection(n) {
     $(sections[activeSectionIdx]).hide();
     activeSectionIdx += n;
@@ -48,4 +51,27 @@ $(document).ready(() => {
     $("#form-password-choose").submit(passwordChooseNext);
     $(".goback-button").click(prevSection);
 
+    $('#password-keystroke-input').keydown((event) => {
+        if (event.repeat) return;
+
+        // if(event.code === "Backspace"){
+        //     passwordKeystrokesTmp = [];
+        //     $('#password-keystroke-input').val('');
+        // }
+
+        else{
+            passwordKeystrokesTmp.push({
+                time: Date.now(),
+                type: "Down",
+                key: event.code
+            });
+            console.log(passwordKeystrokesTmp);
+
+            if(passwordKeystrokesTmp.length === 6){
+                passwordKeystrokes.push(passwordKeystrokesTmp);
+                passwordKeystrokesTmp = [];
+                // counter ++;
+            }
+        }
+    });
 });
