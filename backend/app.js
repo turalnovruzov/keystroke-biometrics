00bd318b77628abb84a59129ff5ccfa46df2d416
Tural Novruzov <tural.nt.1@gmail.com>
2020-12-30 12:35:53 +0400

    Added Subject model and /user/:userEmail path

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const Subject = require('./models/Subject');
 
 const app = express();
 
 mongoose.connect(process.env.DB_CONNECTION, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true
     }, () => console.log('Connected to the database.'));
 
+app.use(express.json());
 
+app.get('/user/:userEmail', async (req, res) => {
+    let user = await Subject.findOne({email: req.params.userEmail});
+    
+    if (user) {
+        res.json({_id: user._id})
+    } else {
+        res.sendStatus(404);
+    }
+})
 
 app.listen(3000);
