5eed097bfee253bcbd04bb8fa44571734357cca6
Tural Novruzov <tural.nt.1@gmail.com>
2020-12-30 10:21:03 +0400

    Connected to the database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () => console.log('Connected to the database.'));
 
 
 
