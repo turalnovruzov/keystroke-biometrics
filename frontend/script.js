49876ea8eacd8a41ad1595bc91c0fdcb1a893d34
Tural Novruzov <tural.nt.1@gmail.com>
2021-02-27 12:24:38 +0400

    Changed password_tries to 3

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 const DOWN = "Down";
 const UP = "Up";
 const BASE_URL = "https://verim.sabanciuniv.edu/datacollectapi";
-const PASSWORD_TRIES = 1;
+const PASSWORD_TRIES = 3;
 
 const sections = ["#section-email", "#section-personal", "#section-password-choose", "#section-password-keystroke", "#section-name-keystroke", "#section-email-keystroke", "#thankyou-section"];
 const passwordRegex = /^[0-9]{6}$/;
